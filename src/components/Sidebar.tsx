313ffbd42caa568225e98c076207da1be5cfe1c3
theoyoth <theobantang1gmail.com>
2023-08-29 19:47:23 +0700

    delete import react and favicon in public

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,13 @@ import LinkSide from './LinkSide'
 const Leagues = [
   { id:1,name:"Premier League",href:"premier-league",emblem:"/img/leagues/premier_league.webp"},
   { id:2,name:"Primera Division",href:"la-liga",emblem:"/img/leagues/laliga.svg"},
-  { id:4,name:"Bundesliga",href:"bundesliga",emblem:"/img/leagues/bundesliga.webp"},
-  { id:3,name:"Serie A",href:"serie-a",emblem:"/img/leagues/serie_a.webp"},
-  { id:4,name:"Ligue 1",href:"ligue-1",emblem:"/img/leagues/ligue_1.webp"},
-  { id:5,name:"Championship",href:"championship",emblem:"/img/leagues/championship.webp"},
-  { id:6,name:"Primeira Liga",href:"primeira-liga",emblem:"/img/leagues/liga_portugal.webp"},
-  { id:7,name:"Brazilian Championship Series A",href:"brazilian-series-a",emblem:"/img/leagues/brazilian_serie_a.webp"},
-  { id:8,name:"Copa Libertadores",href:"copa-libertadores",emblem:"/img/leagues/copa_libertadores.webp"},
+  { id:3,name:"Bundesliga",href:"bundesliga",emblem:"/img/leagues/bundesliga.webp"},
+  { id:4,name:"Serie A",href:"serie-a",emblem:"/img/leagues/serie_a.webp"},
+  { id:5,name:"Ligue 1",href:"ligue-1",emblem:"/img/leagues/ligue_1.webp"},
+  { id:6,name:"Championship",href:"championship",emblem:"/img/leagues/championship.webp"},
+  { id:7,name:"Primeira Liga",href:"primeira-liga",emblem:"/img/leagues/liga_portugal.webp"},
+  { id:8,name:"Brazilian Championship Series A",href:"brazilian-series-a",emblem:"/img/leagues/brazilian_serie_a.webp"},
+  { id:9,name:"Copa Libertadores",href:"copa-libertadores",emblem:"/img/leagues/copa_libertadores.webp"},
 ]
 
 const Sidebar:FC = async() => {
